6073583ca4b92ca460278464b0bfba0eebb275e4
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2025-01-05 18:42:39 +1000

    deployment

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,6 @@
-variable "ecs_cluster_name" {
-  description = "The name of the ECS cluster"
+variable "project_prefix" {
+  description = "The prefix of the project"
   type        = string
-  
 }
 
 variable "task_cpu" {
@@ -16,41 +15,14 @@ variable "task_memory" {
   default     = "512"
 }
 
-variable "cypress_image_tag" {
-  description = "The tag of the testing image to deploy"
-}
-
-variable "backend_image_tag" {
-  description = "The tag of the backend image to deploy"
-}
-
-variable "frontend_image_tag" {
-  description = "The tag of the frontend image to deploy"
-}
-
-variable "backend_repository_name" {
-  description = "Name of the backend repo"
-  type        = string
-}
-
-variable "frontend_repository_name" {
-  description = "Name of the frontend repo"
-  type        = string
-}
-
-variable "cypress_repository_name" {
-  description = "Name of the testing repo"
-  type        = string
-}
-
 variable "iam_role" {
-  type        = string
   description = "Self-hosted runner EC2 instance role"
+  type        = string
 }
 
 variable "lifecycle_policy" {
-  type        = string
   description = "the lifecycle policy to be applied to the ECR repos"
+  type        = string
 }
 
 variable "aws_account_id" {
